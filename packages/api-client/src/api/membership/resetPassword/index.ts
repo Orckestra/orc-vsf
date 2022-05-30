7d9ec0264abfeb225890de16960dd0bfea8d0d2e
Olya Ignatenko <olga.ignatenko@orckestra.com>
2022-05-30 08:56:21 +0300

    lint update

--- packages/api-client/src/api/membership/resetPassword/index.ts
+++ packages/api-client/src/api/membership/resetPassword/index.ts
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export default async function resetPassword(context, params) {
-    const { email, password, passwordAnswer, username } = params;
-    const { api, scope } = context.config;
-    const url = new URL(`/api/membership/${scope}/resetPassword`, api.url);
-    const { data } = await context.client.post(url.href, {
-      email,
-      username,
-      passwordAnswer,
-      password
-    });
-    return data;
-  }
+  const { email, password, passwordAnswer, username } = params;
+  const { api, scope } = context.config;
+  const url = new URL(`/api/membership/${scope}/resetPassword`, api.url);
+  const { data } = await context.client.post(url.href, {
+    email,
+    username,
+    passwordAnswer,
+    password
+  });
+  return data;
+}
