09e5fbbb8e803383c4d91de582158e76fe58a219
Inna Boitsun <inna.boitsun@orckestra.com>
2022-05-31 11:56:14 +0300

    do not allow login when secret phase is not configured

--- packages/api-client/src/api/membership/login/index.ts
+++ packages/api-client/src/api/membership/login/index.ts
@@ -7,6 +7,7 @@ export default async function login(context, params) {
   const { password, username } = params;
   if (myAccount.secretPassphrase === undefined || myAccount.secretPassphrase === '') {
     console.error('Secret Passphrase is not configured. For security purpose it is important to configure Secret Passphrase.');
+    return {errorMessage: 'Secret passphrase is not configured.'};
   }
 
   const url = new URL(
