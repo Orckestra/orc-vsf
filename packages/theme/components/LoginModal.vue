a83b8a2ba223d927ccce41877254741def0714df
Olya Ignatenko <olga.ignatenko@orckestra.com>
2022-06-01 11:35:53 +0300

    update

--- packages/theme/components/LoginModal.vue
+++ packages/theme/components/LoginModal.vue
@@ -250,9 +250,10 @@ export default {
     extend('password', {
       ...password,
       validate(value) {
+        console.log()
         const minRequiredPasswordLength = configurationGetters.getMinRequiredPasswordLength(configuration.value);
         const minRequiredNonAlphanumericCharacters = configurationGetters.getMinRequiredNonAlphanumericCharacters(configuration.value);
-        return new RegExp(`^(?=.*?[#?!@$%^&*+-_)()]{${minRequiredNonAlphanumericCharacters}}).{${minRequiredPasswordLength},}$`).test(value);
+        return new RegExp(`^(?=.*?[#?!@$%^&*+-]{${minRequiredNonAlphanumericCharacters}}).{${minRequiredPasswordLength},}$`).test(value);
       },
       message: 'Your password must have a minimum 6 characters including at least 1 special character'
     });
