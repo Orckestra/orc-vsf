c318de537f5f60ea1f7e95e37c458703b37c41bb
Olya Ignatenko <olga.ignatenko@orckestra.com>
2022-06-09 15:52:52 +0300

    update

--- packages/composables/src/helpers/responseUtils.ts
+++ packages/composables/src/helpers/responseUtils.ts
@@ -1,8 +1,8 @@
-export const checkResponseForError = (response: any): void => {
+import type { ResponseData } from '../types';
+export const checkResponseForError = (response: ResponseData): void => {
   if (response?.responseStatus?.errorCode) {
     const error = new Error(response.responseStatus.message);
     error.name = response.responseStatus.errorCode;
     throw error;
   }
 };
-  
