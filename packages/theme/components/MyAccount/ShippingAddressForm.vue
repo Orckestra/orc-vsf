4bbe9e11a1329567a12459bed3e7489872d3f111
Alex Honcharuk <olek.honcharuk@orckestra.com>
2022-06-10 13:29:08 +0300

    add form

---
+++ packages/theme/components/MyAccount/ShippingAddressForm.vue
@@ -0,0 +1,312 @@
+<template>
+<!--  <SfShipping
+    :shippingMethods='[{"isOpen":false,"price":"Free","delivery":"Delivery from 3 to 7 business days","label":"Pickup in the store","value":"store","description":"Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted."},{"isOpen":false,"price":"$9.90","delivery":"Delivery from 4 to 6 business days","label":"Delivery to home","value":"home","description":"Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted."},{"isOpen":false,"price":"$9.90","delivery":"Delivery from 4 to 6 business days","label":"Paczkomaty InPost","value":"inpost","description":"Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted."},{"isOpen":false,"price":"$11.00","delivery":"Delivery within 48 hours","label":"48 hours coffee","value":"coffee","description":"Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted."},{"isOpen":false,"price":"$14.00","delivery":"Delivery within 24 hours","label":"Urgent 24h","value":"urgent","description":"Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted."}]'
+    headingTitle="Shipping"
+    :headingTitleLevel="2"
+    :inputsLabels='["First name","Last name","Street name","City","State/Province","Zip-code","Phone number"]'
+    selectLabel="Country"
+    :countries='["Austria","Azerbaijan","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia","Finland","France","Georgia","Germany","Greece","Hungary","Iceland","Ireland","Italy","Kosovo","Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta","Moldova","Monaco","Montenegro","The Netherlands","Norway","Poland","Portugal","Romania","Russia","San Marino","Serbia","Slovakia","Slovenia","Spain","Sweden","Switzerland","Turkey","Ukraine","United Kingdom","Vatican City"]'
+    methodsHeadingTitle="Shipping method"
+    :methodsHeadingTitleLevel="2"
+  />-->
+  <ValidationObserver v-slot="{ handleSubmit, reset }">
+    <form
+      class="form"
+      @submit.prevent="handleSubmit(submitForm(reset))"
+    >
+      <div class="form__horizontal">
+        <ValidationProvider
+          v-slot="{ errors }"
+          rules="required|min:2|max:36"
+          class="form__element"
+        >
+          <SfInput
+            v-model="form.firstname"
+            name="firstName"
+            :label="'First Name'"
+            required
+            :valid="!errors[0]"
+            :error-message="errors[0]"
+          />
+        </ValidationProvider>
+        <ValidationProvider
+          v-slot="{ errors }"
+          rules="required|min:2|max:36"
+          class="form__element"
+        >
+          <SfInput
+            v-model="form.lastname"
+            name="lastName"
+            :label="'Last Name'"
+            required
+            :valid="!errors[0]"
+            :error-message="errors[0]"
+          />
+        </ValidationProvider>
+      </div>
+      <ValidationProvider
+        v-slot="{ errors }"
+        rules="required|min:2|max:36"
+        class="form__element"
+      >
+        <SfInput
+          v-model="form.streetName"
+          name="streetName"
+          :label="'Street Name'"
+          required
+          :valid="!errors[0]"
+          :error-message="errors[0]"
+        />
+      </ValidationProvider>
+      <ValidationProvider
+        v-slot="{ errors }"
+        rules="required|max:6"
+        class="form__element"
+      >
+        <SfInput
+          v-model="form.houseNumber"
+          name="houseNumber"
+          :label="'House/Apartment number'"
+          required
+          :valid="!errors[0]"
+          :error-message="errors[0]"
+        />
+      </ValidationProvider>
+      <div class="form__horizontal">
+        <ValidationProvider
+          v-slot="{ errors }"
+          rules="required|min:2|max:36"
+          class="form__element"
+        >
+          <SfInput
+            v-model="form.city"
+            name="city"
+            :label="'City'"
+            required
+            :valid="!errors[0]"
+            :error-message="errors[0]"
+          />
+        </ValidationProvider>
+        <ValidationProvider
+          v-slot="{ errors }"
+          rules="required|min:2|max:36"
+          class="form__element"
+        >
+          <SfInput
+            v-model="form.state"
+            name="state"
+            :label="'State/Province'"
+            required
+            disabled
+            :valid="!errors[0]"
+            :error-message="errors[0]"
+          />
+        </ValidationProvider>
+      </div>
+      <div class="form__horizontal">
+        <ValidationProvider
+          v-slot="{ errors }"
+          rules="required|min:4|max:8"
+          class="form__element"
+        >
+          <SfInput
+            v-model="form.zipCode"
+            name="zipCode"
+            :label="'Zip-code'"
+            required
+            :valid="!errors[0]"
+            :error-message="errors[0]"
+          />
+        </ValidationProvider>
+        <ValidationProvider
+          v-slot="{ errors }"
+          rules="required"
+          class="form__element"
+        >
+          <SfSelect
+            v-model="form.country"
+            name="country"
+            :label="'Country'"
+            class=" form__select  sf-select--underlined"
+            required
+            :valid="!errors[0]"
+            :error-message="errors[0]"
+          >
+            <SfSelectOption
+              v-for="{key, label} in countries"
+              :key="key"
+              :value="key"
+            >
+              {{ label }}
+            </SfSelectOption>
+          </SfSelect>
+        </ValidationProvider>
+      </div>
+      <ValidationProvider
+        v-slot="{ errors }"
+        rules="required|min:8|max:10"
+        class="form__element"
+      >
+        <SfInput
+          v-model="form.phoneNumber"
+          name="phoneNumber"
+          :label="'Phone number'"
+          required
+          :valid="!errors[0]"
+          :error-message="errors[0]"
+        />
+      </ValidationProvider>
+
+      <SfCheckbox
+        v-model="form.default"
+        name="default"
+        label="Set as default"
+        class="form__element form__checkbox"
+      />
+
+      <SfButton
+        class="action-button"
+        >
+        {{ $t('Add new address') }}
+      </SfButton>
+    </form>
+  </ValidationObserver>
+</template>
+
+<script>
+import { defineComponent, ref } from '@nuxtjs/composition-api';
+import { useUiNotification } from '~/composables';
+import { SfButton, SfInput, SfModal, SfSelect, SfCheckbox } from '@storefront-ui/vue';
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
+
+const COUNTRIES = [
+  { key: 'US', label: 'United States' },
+  { key: 'UK', label: 'United Kingdom' },
+  { key: 'IT', label: 'Italy' },
+  { key: 'PL', label: 'Poland' }
+];
+
+export default {
+  name: 'ShippingAddressForm',
+  components: {
+    SfInput,
+    SfButton,
+    SfSelect,
+    SfCheckbox,
+    // SfModal,
+    ValidationProvider,
+    ValidationObserver
+  },
+  emits: ['submit'],
+  setup(props, { emit }) {
+    // const { user, error: userError } = useUser();
+  //  const currentPassword = ref('');
+    //   const requirePassword = ref(false);
+    //    const resetForm = () => ({
+    //      firstname: userGetters.getFirstName(user.value),
+    //      lastname: userGetters.getLastName(user.value),
+    //      email: userGetters.getEmailAddress(user.value)
+    //    });
+    const { send: sendNotification } = useUiNotification();
+    const form = ref({
+      firstname: '',
+      lastname: '',
+      streetName: '',
+      houseNumber: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      country: '',
+      phoneNumber: '',
+      default: false
+    });
+
+    const submitForm = (resetValidationFn) => async () => {
+      // const onComplete = async () => {
+      //   form.value = resetForm();
+      //   requirePassword.value = false;
+      //   currentPassword.value = '';
+      //   if (userError.value.updateUser) {
+      //     sendNotification({
+      //       id: Symbol('user_updated_error'),
+      //       message: userError.value.updateUser.message,
+      //       type: 'danger',
+      //       icon: 'error',
+      //       persist: false,
+      //       title: 'User Account'});
+      //   } else {
+      //     sendNotification({
+      //       id: Symbol('user_updated'),
+      //       message: 'The user account data was successfully updated!',
+      //       type: 'success',
+      //       icon: 'check',
+      //       persist: false,
+      //       title: 'User Account'
+      //     });
+      //   }
+      //   resetValidationFn();
+      // };
+      //
+      // const onError = () => {
+      //   form.value = resetForm();
+      //   requirePassword.value = false;
+      //   currentPassword.value = '';
+      //   if (userError.value.updateUser) {
+      //     sendNotification({
+      //       id: Symbol('user_updated_error'),
+      //       message: userError.value.updateUser.message,
+      //       type: 'danger',
+      //       icon: 'error',
+      //       persist: false,
+      //       title: 'User Account'});
+      //   }
+      // };
+
+      emit('submit', {
+        form
+        // onComplete, onError
+      });
+    };
+
+    return {
+      //   requirePassword,
+    //  currentPassword,
+      countries: COUNTRIES,
+      form,
+      submitForm
+    };
+  }
+};
+</script>
+<style lang='scss' scoped>
+.form {
+  &__element {
+    display: block;
+    margin: 0 0 var(--spacer-lg) 0;
+  }
+  &__button {
+    display: block;
+    width: 100%;
+    @include for-desktop {
+      width: 17.5rem;
+    }
+  }
+  &__horizontal {
+    @include for-desktop {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .form__element {
+      @include for-desktop {
+        flex: 1;
+        margin-right: var(--spacer-2xl);
+      }
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+}
+</style>
