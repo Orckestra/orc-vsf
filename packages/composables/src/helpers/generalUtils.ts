80d237eabfedfa04ef3a1a7ad45218916866ede8
Inna Boitsun <inna.boitsun@orckestra.com>
2022-06-14 11:58:31 +0300

    Merge Carts after login AB#62019

--- packages/composables/src/helpers/generalUtils.ts
+++ packages/composables/src/helpers/generalUtils.ts
@@ -1,5 +1,21 @@
+import {
+  Context
+} from '@vue-storefront/core';
 export const isGuidEmpty = (guid: string): boolean => {
   return (!guid ||
     guid === '00000000-0000-0000-0000-000000000000' ||
     guid === '00000000000000000000000000000000');
 };
+
+export const getUserToken = (context: Context): string => {
+  const app = context.$occ.config.app;
+  const appKey = app.$config.appKey;
+  return app.$cookies.get(appKey + '_token');
+}
+
+export const setUserToken = (context: Context, userToken: string, opts: any = {}): void => {
+  const app = context.$occ.config.app;
+  const appKey = app.$config.appKey;
+  app.$cookies.set(appKey + '_token', userToken, opts);
+}
+
