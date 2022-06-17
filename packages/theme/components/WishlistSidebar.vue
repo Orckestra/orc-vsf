69068635e7c03cf7fe4e2f985c84c7ddff060ea7
Olya Ignatenko <olga.ignatenko@orckestra.com>
2022-06-17 10:27:35 +0300

    update

--- packages/theme/components/WishlistSidebar.vue
+++ packages/theme/components/WishlistSidebar.vue
@@ -45,7 +45,7 @@
           </div>
           <div class="sidebar-bottom">
           <SfProperty class="sf-property--full-width my-wishlist__total-price">
-            <template #name"{}">&nbsp;</template>
+            <template #name="{}">&nbsp;</template>
             <template #value="{}">&nbsp;</template>
           </SfProperty>
           </div>
