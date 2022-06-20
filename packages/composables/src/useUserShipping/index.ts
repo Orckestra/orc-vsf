b76578db7440d59b2182a437545716a67de58eb6
Alex Honcharuk <olek.honcharuk@orckestra.com>
2022-06-20 09:44:59 +0300

    fix update address list

--- packages/composables/src/useUserShipping/index.ts
+++ packages/composables/src/useUserShipping/index.ts
@@ -8,50 +8,41 @@ import type {
 } from '@vue-storefront/orc-vsf-api';
 import { getUserToken } from '../helpers/generalUtils';
 
-const params: UseUserShippingFactoryParams<AddressItem[], AddressItem> = {
+const factoryParams: UseUserShippingFactoryParams<AddressItem[], AddressItem> = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   addAddress: async (context: Context, params) => {
-    const {shipping, address} = params;
+    const {address} = params;
     const userToken = getUserToken(context);
     if ((userToken === undefined || userToken === '')) {
       return null;
     }
 
-    const addedAddress = await context.$occ.api.addUserAddress({ userToken, address });
-    if (addedAddress.isPreferredShipping) {
-      return [...shipping.map(a => ({ ...a, isPreferredShipping: false })), addedAddress];
-    }
-    return [...shipping, addedAddress];
+    await context.$occ.api.addUserAddress({ userToken, address });
+    return factoryParams.load(context, params);
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   deleteAddress: async (context: Context, params) => {
-    const {shipping, address} = params;
+    const {address} = params;
     const userToken = getUserToken(context);
     if ((userToken === undefined || userToken === '')) {
       return null;
     }
 
-    const data = await context.$occ.api.deleteUserAddress({ userToken, addressId: address.id });
-    const addresses = [...shipping];
-    if (data !== null) {
-      addresses.splice(shipping.findIndex(a => a.id === address.id), 1);
-    }
-    return addresses;
+    await context.$occ.api.deleteUserAddress({ userToken, addressId: address.id });
+    return factoryParams.load(context, params);
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   updateAddress: async (context: Context, params) => {
-    const {shipping, address} = params;
+    const {address} = params;
     const userToken = getUserToken(context);
     if ((userToken === undefined || userToken === '')) {
       return null;
     }
 
-    const updatedAddress = await context.$occ.api.updateUserAddress({ userToken, address, addressId: address.id });
-    const addresses = updatedAddress.isPreferredShipping ? shipping.map(a => ({ ...a, isPreferredShipping: false })) : [...shipping];
-    addresses.splice(shipping.findIndex(a => a.id === updatedAddress.id), 1, updatedAddress);
-    return addresses;
+    await context.$occ.api.updateUserAddress({ userToken, address, addressId: address.id });
+    return factoryParams.load(context, params);
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -66,17 +57,15 @@ const params: UseUserShippingFactoryParams<AddressItem[], AddressItem> = {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setDefaultAddress: async (context: Context, params) => {
-    const {shipping, address} = params;
+    const {address} = params;
     const userToken = getUserToken(context);
     if ((userToken === undefined || userToken === '')) {
       return null;
     }
 
-    const updatedAddress = await context.$occ.api.updateUserAddress({ userToken, address: { ...address, isPreferredShipping: true }, addressId: address.id });
-    const addresses = shipping.map(a => ({ ...a, isPreferredShipping: false }));
-    addresses.splice(shipping.findIndex(a => a.id === updatedAddress.id), 1, updatedAddress);
-    return addresses;
+    await context.$occ.api.updateUserAddress({ userToken, address: { ...address, isPreferredShipping: true }, addressId: address.id });
+    return factoryParams.load(context, params);
   }
 };
 
-export const useUserShipping = useUserShippingFactory<AddressItem[], AddressItem>(params);
+export const useUserShipping = useUserShippingFactory<AddressItem[], AddressItem>(factoryParams);
