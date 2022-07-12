3f180f5268d45765dd0e81a7591b880b7e624c7a
Alex Honcharuk <olek.honcharuk@orckestra.com>
2022-07-12 15:00:09 +0300

    fulfillment types order

--- packages/composables/src/useFulfillmentMethods/index.ts
+++ packages/composables/src/useFulfillmentMethods/index.ts
@@ -2,14 +2,18 @@ import { Context } from '@vue-storefront/core';
 import { useFulfillmentMethodsFactory } from '../factories/useFulfillmentMethodsFactory';
 import type { FulfillmentMethod } from '@vue-storefront/orc-vsf-api';
 import { getUserToken } from '../helpers/generalUtils';
+import { FulfillmentMethodType } from '@vue-storefront/orc-vsf-api';
 
 export const useFulfillmentMethods = useFulfillmentMethodsFactory<FulfillmentMethod>({
-  load: (context: Context) => {
+  load: async (context: Context) => {
     const userToken = getUserToken(context);
     if ((userToken === undefined || userToken === '')) {
       return null;
     }
 
-    return context.$occ.api.getFulfillmentMethods({ userToken });
+    const items = await context.$occ.api.getFulfillmentMethods({ userToken });
+    const typeOrder = [FulfillmentMethodType.Shipping, FulfillmentMethodType.PickUp];
+
+    return items.sort((a, b) => (typeOrder.indexOf(a.fulfillmentMethodType) - typeOrder.indexOf(b.fulfillmentMethodType)) || a.cost - b.cost);
   }
 });
