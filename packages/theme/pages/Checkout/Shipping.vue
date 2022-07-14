5908504150d061999006e505c79c1ff680e0d296
Alex Honcharuk <olek.honcharuk@orckestra.com>
2022-07-14 13:03:42 +0300

    63410 update list of address

--- packages/theme/pages/Checkout/Shipping.vue
+++ packages/theme/pages/Checkout/Shipping.vue
@@ -141,7 +141,7 @@ import {
   SfRadio,
   SfIcon
 } from '@storefront-ui/vue';
-import { computed, ref, useRouter } from '@nuxtjs/composition-api';
+import { computed, ref, useRouter, watch } from '@nuxtjs/composition-api';
 import { useUiNotification } from '~/composables';
 import { onSSR } from '@vue-storefront/core';
 import { useUser, useFulfillmentMethods, useUserAddresses, useCart, cartGetters, fulfillmentMethodsGetters, userAddressGetters } from '@vue-storefront/orc-vsf';
@@ -317,6 +317,12 @@ export default {
       loadUserShipping(),
       loadFulfillmentMethods()
     ]));
+
+    watch(isAuthenticated, () => {
+      if (isAuthenticated.value) {
+        loadUserShipping();
+      }
+    });
 
     const goBack = () => {
       router.push(context.root.localePath({ name: 'personalDetails' }));
