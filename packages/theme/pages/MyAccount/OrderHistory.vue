c628e7a6315dfc49d2b7cb8dcae25c5f399c240f
Olya Ignatenko <olga.ignatenko@orckestra.com>
2022-07-15 13:53:48 +0300

    fixes about the comments

--- packages/theme/pages/MyAccount/OrderHistory.vue
+++ packages/theme/pages/MyAccount/OrderHistory.vue
@@ -5,22 +5,22 @@
       <SfProperty
         name="Order Number"
         :value="orderGetters.getNumber(currentOrder)"
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
       <SfProperty
         name="Date"
         :value="orderGetters.getDate(currentOrder)"
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
       <SfProperty
         name="Status"
         :value="getOrderStatusLookup(currentOrder)" 
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
       <SfProperty
         name="Total"
         :value="$n(orderGetters.getPrice(currentOrder), 'currency')"
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
     </div>
     <SfTable class="sf-table--bordered table">
@@ -67,16 +67,16 @@
       <SfProperty
         name="Shipping method"
         :value="th.getTranslation(orderGetters.getFulfillmentMethodName(currentOrder))"
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
       <SfProperty
         name="Status"
         :value="getShipmentStatusLookup(currentOrder)"
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
       <SfProperty
         name="Shiping address"
-        class="sf-property--full-width">
+        class="sf-property">
         <template #value>
           <AddressPreview :address="orderGetters.getShippingAddress(currentOrder)" />
         </template>
@@ -91,7 +91,7 @@
       <SfProperty
         name="Payment method"
         :value="orderGetters.getPaymentMethod(currentOrder)"
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
       <SfProperty
         name="Billing address"
@@ -111,92 +111,97 @@
       <SfProperty
         name="Subtotal"
         :value="orderGetters.getSubTotal(currentOrder)"
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
       <div v-for="tax in orderGetters.getTaxes(currentOrder)">
         <SfProperty
           :name="th.getTranslation(orderGetters.getTaxName(tax))"
           :value="orderGetters.getTaxTotal(tax)"
-          class="sf-property--full-width property"
+          class="sf-property property"
         />
       </div>
       <SfProperty
+        :name="$t('Shipping')"
+        :value="$n(orderGetters.getShippingPrice(currentOrder), 'currency')"
+        class="sf-property property"
+      />
+      <SfProperty
         name="Total"
         :value="orderGetters.getTotal(currentOrder)"
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
       <SfProperty
         name="Saving"
         :value="$n(orderGetters.getSaving(currentOrder), 'currency')"
-        class="sf-property--full-width property"
+        class="sf-property property"
       />
     </div>
   </div>
   <div v-else>
-    <SfTabs :open-tab="1">
-      <SfTab title="My сurrent orders">          
-        <p class="message">
-          {{ $t('Check the details and status of your orders in the online store. You can also cancel your order or request a return.') }}
-        </p>
-        <div v-if="totalOrdersCurrent === 0" class="no-orders">
-          <p class="no-orders__title">{{ $t('You currently have no orders') }}</p>
-          <SfButton class="no-orders__button">{{ $t('Start shopping') }}</SfButton>
-        </div>
-        <SfTable v-else class="orders">
-          <SfTableHeading>
-            <SfTableHeader
-              v-for="tableHeader in tableHeadersCurrent"
-              :key="tableHeader"
-            >{{ tableHeader }}</SfTableHeader>
-            <SfTableHeader class="orders__element--right" />
-          </SfTableHeading>
-          <SfTableRow v-for="order in currentOrders" :key="ordersHistoryGetters.getId(order)">
-            <SfTableData v-e2e="'order-number'">{{ ordersHistoryGetters.getNumber(order) }}</SfTableData>
-            <SfTableData>{{ ordersHistoryGetters.getDate(order) }}</SfTableData>
-            <SfTableData>{{ $n(ordersHistoryGetters.getPrice(order), 'currency') }}</SfTableData>
-            <SfTableData>
-              <span :class="getStatusTextClass(order)">{{ getOrderStatusLookup(order) }}</span>
-            </SfTableData>
-            <SfTableData class="orders__view orders__element--right">
-              <SfButton class="sf-button--text desktop-only" @click="getOrderDetails(order)">
-                {{ $t('View details') }}
-              </SfButton>
-            </SfTableData>
-          </SfTableRow>
-        </SfTable>
-        <p>Total orders - {{ totalOrdersCurrent }}</p>
-      </SfTab>
-      <SfTab title="My past orders">
-        <div>
-          <p class="message">
-              {{ $t('Your Past orders') }}
-          </p>
-          <SfTable class="orders">
-            <SfTableHeading>
-              <SfTableHeader
-                v-for="tableHeader in tableHeadersPast"
-                :key="tableHeader"
-              >{{ tableHeader }}</SfTableHeader>
-              <SfTableHeader class="orders__element--right" />
-            </SfTableHeading>
-            <SfTableRow v-for="pastOrder in pastOrders" :key="ordersHistoryGetters.getId(pastOrder)">
-              <SfTableData v-e2e="'order-number'">{{ ordersHistoryGetters.getNumber(pastOrder) }}</SfTableData>
-              <SfTableData>{{ ordersHistoryGetters.getDate(pastOrder) }}</SfTableData>
-              <SfTableData>
-                <span :class="getStatusTextClass(pastOrder)">{{ getOrderStatusLookup(pastOrder) }}</span>
-              </SfTableData>
-              <SfTableData class="orders__view orders__element--right">
-                <SfButton class="sf-button--text desktop-only" @click="getOrderDetails(pastOrder)">
-                  {{ $t('View details') }}
-                </SfButton>
-              </SfTableData>
-            </SfTableRow>
-          </SfTable>
-          <p>Total orders Past - {{ totalOrdersPast }}</p>
-        </div>
-      </SfTab>
-    </SfTabs>
-  </div>  
+    <p class="message">
+      {{ $t('Check the details and status of your orders in the online store. You can also cancel your order or request a return.') }}
+    </p>
+    <div v-if="totalOrders === 0" class="no-orders">
+      <p class="no-orders__title">{{ $t('You currently have no orders') }}</p>
+      <SfButton class="no-orders__button">{{ $t('Start shopping') }}</SfButton>
+    </div>
+    <div v-else>
+      <SfTable class="orders">
+        <SfTableHeading>
+          <SfTableHeader
+            v-for="tableHeader in tableHeaders"
+            :key="tableHeader"
+          >{{ tableHeader }}</SfTableHeader>
+          <SfTableHeader class="orders__element--right" />
+        </SfTableHeading>
+        <SfTableRow v-for="order in orders" :key="ordersHistoryGetters.getId(order)">
+          <SfTableData v-e2e="'order-number'">{{ ordersHistoryGetters.getNumber(order) }}</SfTableData>
+          <SfTableData>{{ ordersHistoryGetters.getDate(order) }}</SfTableData>
+          <SfTableData>{{ $n(ordersHistoryGetters.getPrice(order), 'currency') }}</SfTableData>
+          <SfTableData>
+            <span :class="getStatusTextClass(order)">{{ getOrderStatusLookup(order) }}</span>
+          </SfTableData>
+          <SfTableData class="orders__view orders__element--right">
+            <SfButton class="sf-button--text desktop-only" @click="getOrderDetails(order)">
+              {{ $t('View details') }}
+            </SfButton>
+          </SfTableData>
+        </SfTableRow>
+      </SfTable>
+      <p>My  orders ({{ totalOrders }})</p>
+      <LazyHydrate on-interaction>
+        <SfPagination
+          v-if="!loading"
+          class="products__pagination desktop-only"
+          v-show="pagination.totalPages > 1"
+          :current="pagination.currentPage"
+          :total="pagination.totalPages"
+          :visible="5"
+        />
+      </LazyHydrate>
+      <div
+        v-show="pagination.totalPages > 1"
+        class="products__show-on-page">
+        <span class="products__show-on-page__label">{{ $t('Show on page') }}</span>
+        <LazyHydrate on-interaction>
+          <SfSelect
+            :value="pagination && pagination.itemsPerPage ? pagination.itemsPerPage.toString() : ''"
+            class="products__items-per-page"
+            @input="th.changeItemsPerPage"
+          >
+            <SfSelectOption
+              v-for="option in pagination.pageOptions"
+              :key="option"
+              :value="option"
+              class="products__items-per-page__option"
+            >
+              {{ option }}
+            </SfSelectOption>
+          </SfSelect>
+        </LazyHydrate>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -208,17 +213,20 @@ import {
   SfLink,
   SfHeading,
   SfImage,
-  SfPrice
+  SfPrice,
+  SfPagination,
+  SfSelect  
 } from '@storefront-ui/vue';
 import AddressPreview from '../../components/AddressPreview';
 import { computed, ref } from '@nuxtjs/composition-api';
-import { useOrdersHistory, ordersHistoryGetters, orderGetters, useMetadata, metadataGetters, useOrder } from '@vue-storefront/orc-vsf';
+import { useUserOrder, ordersHistoryGetters, orderGetters, useMetadata, metadataGetters, useOrder } from '@vue-storefront/orc-vsf';
 import { useUiHelpers } from '~/composables';
 import { useRouter } from '@nuxtjs/composition-api';
 import { AgnosticOrderStatus, addBasePath } from '@vue-storefront/core';
 import { onSSR } from '@vue-storefront/core';
+import LazyHydrate from 'vue-lazy-hydration';
 export default {
-  name: 'PersonalDetails',
+  name: 'OrdersHistory',
   components: {
     SfTabs,
     SfTable,
@@ -228,56 +236,40 @@ export default {
     AddressPreview,
     SfHeading,
     SfImage,
-    SfPrice
+    SfPrice,
+    SfPagination,
+    LazyHydrate,
+    SfSelect
   },
   setup() {
     const th = useUiHelpers();
-    const { response: orderHistoryCurrent, load: loadOrdersHistoryCurrent } = useOrdersHistory('order-history-current');
-    const { response: orderHistoryPast, load: loadOrdersHistoryPast } = useOrdersHistory('order-history-past');
+    const { orders: orderHistory, search, loading } = useUserOrder('order-history');
     const { response: orderByNumber, find: getOrderByNumber } = useOrder();
     const { response: metadata } = useMetadata();
-    
     const router = useRouter();
     const { locale } = router.app.$i18n;
-    
+    const orders = ref(null);
     const currentOrder = ref(null);
     let isOrderSelected =  ref(false);
+
     onSSR(async () => {
-      if(!orderHistoryCurrent.value?.results){
-        await loadOrdersHistoryCurrent({ orderTense: 1 });
-      }
-      if(!orderHistoryPast.value){
-        await loadOrdersHistoryPast({ orderTense: 0 });    
-      }
+      const facetsFromUrl = th.getFacetsFromURL();
+      await search({ page: facetsFromUrl.page, itemsPerPage: facetsFromUrl.itemsPerPage, filterMember: 'OrderStatus', filterValues: ['PendingProcess','InProgress','PartiallyFulfilled', 'New', 'Completed', 'Canceled','Shipped'] });
     });    
 
-    
-    const tableHeadersCurrent = [
-      "Order Number",
-      "Creation Date",
-      "Order Total",
-      "Order Status",
-    ];
-
-    const tableHeadersPast = [
-      "Order Number",
-      "Creation Date",
-      "Order Status",
-    ];
-
     const getStatusTextClass = (order) => {
-      const status = ordersHistoryGetters.getStatus(order);
-      switch (status) {
-        case 'InProgress':
-          return 'text-warning';
-        case 'PendingProcess':
-          return 'text-warning';
-        case 'Finalised':
-          return 'text-success';
-        default:
-          return '';
-      }
-    };
+     const status = ordersHistoryGetters.getStatus(order);
+     switch (status) {
+       case 'InProgress':
+         return 'text-warning';
+       case 'Completed':
+         return 'text-success';
+       case 'Canceled':
+         return 'text-danger';
+       default:
+         return 'text-info';
+     }
+   };
 
     const getOrderDetails = async (order) => {      
       await getOrderByNumber({orderNumber: ordersHistoryGetters.getNumber(order)});
@@ -296,30 +288,24 @@ export default {
       return metadataGetters.getLookupValueDisplayName(metadata?.value, lookupName , value, locale);
     }
 
-    const currentOrders = computed(() => ordersHistoryGetters.getOrdersHistory(orderHistoryCurrent.value));
-    const pastOrders = computed(() => ordersHistoryGetters.getOrdersHistory(orderHistoryPast.value));
-    
+    const pagination = computed(() => ordersHistoryGetters.getPagination(orderHistory.value, th.getFacetsFromURL()));
+
     return {
-      tableHeadersCurrent,
-      tableHeadersPast,
-      currentOrders,
-      totalOrdersCurrent: computed(() => ordersHistoryGetters.getOrdersTotal(orderHistoryCurrent.value)),
-      totalOrdersPast: computed(() => ordersHistoryGetters.getOrdersTotal(orderHistoryPast.value)),
-      pastOrders,
+      orders: computed(() => ordersHistoryGetters.getOrdersHistory(orderHistory?.value)),
+      totalOrders: computed(() => ordersHistoryGetters.getOrdersTotal(orderHistory?.value)),
       getStatusTextClass,
       orderGetters,
       ordersHistoryGetters,
-      currentOrder: computed(() => orderByNumber.value),
+      currentOrder: computed(() => orderByNumber?.value),
       getOrderDetails,
       isOrderSelected,
       th,
-      metadataGetters,
-      metadata,
-      locale,
       getOrderStatusLookup,
       addBasePath,
       getShipmentStatusLookup,
-      tableHeaders: ['Description', 'Unit Price', 'Quantity', 'Subtotal']
+      tableHeaders: ['Description', 'Unit Price', 'Quantity', 'Subtotal'],
+      pagination,
+      loading
     };
   }
 };
