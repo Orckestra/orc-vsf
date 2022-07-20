c7892e54f121ac77e7f013f9fa9b5b3b44a1dd4f
Inna Boitsun <inna.boitsun@orckestra.com>
2022-07-20 15:32:06 +0300

    lint errors

--- packages/theme/components/Checkout/VsfPaymentProvider.vue
+++ packages/theme/components/Checkout/VsfPaymentProvider.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
-    <SfRadio
-      v-e2e="'payment-method'"
-      v-for="method in methods"
-      :key="method.id"
-      :label="th.getTranslation(method.displayName)"
-      :value="method.id"
-      :disabled="loading"
-      :selected ="selectedMethod"
-      name="spaymentMethod"
-      class="form__radio payment"
-      @change="selectMethod(method.id)"
-    >
-      <div class="payment__label">
-        {{ th.getTranslation(method.displayName) }}
-      </div>
-    </SfRadio>
+    <div class="payment-methods">
+      <SfRadio
+        v-e2e="'payment-method'"
+        v-for="method in methods"
+        :key="method.id"
+        :label="th.getTranslation(method.displayName)"
+        :value="method.id"
+        :disabled="loading"
+        :selected ="selectedMethod"
+        name="spaymentMethod"
+        class="form__radio payment-method"
+        :class="{'payment-method-applepay' : method.id === bamboraApplePayMethodId }"
+        @change="selectMethod(method.id)"
+      >
+        <div class="payment__label">
+          {{ th.getTranslation(method.displayName) }}
+        </div>
+      </SfRadio>
+    </div>
+    <transition name="sf-fade">
+        <div v-if="isCreditCard" class="credit-card-form">
+          <p>TODO Credit Card form</p>
+        </div>
+    </transition>
   </div>
 </template>
 
@@ -35,21 +43,23 @@ export default {
   },
 
   setup(props, { emit }) {
-
+    const bamboraApplePayMethodId = '084dbf29e00d4709ad4ec8c7562cfefd';
     const { methods: onsiteMethods, load: loadOnsiteMethods } = usePaymentMethods('OnsiteMethods');
     const { methods: bamboraMethods, load: loadBamboraMethods } = usePaymentMethods('BamboraMethods');
     const { cart, updatePaymentMethod, loading } = useCart();
     const validMethods = computed(() => {
+      if (!bamboraMethods.value || !onsiteMethods.value) return [];
       const allMethods = bamboraMethods.value.concat(onsiteMethods.value);
       return paymentMethodGetters.getValidPaymentMethods(allMethods);
     });
     const defaultMethod = computed(() => paymentMethodGetters.getDefaultMethod(validMethods.value));
     const payment = computed(() => cartGetters.getActivePayment(cart.value));
     const selectedMethod = computed(() => payment.value?.paymentMethod?.id);
+    const isCreditCard = computed(() => payment.value?.paymentMethod?.type === 'CreditCard');
     const isBilling = computed(() => cartGetters.isBillingReady(cart.value));
     const th = useUiHelpers();
 
-    if (selectedMethod) {
+    if (selectedMethod.value) {
       emit('status');
     }
 
@@ -57,6 +67,7 @@ export default {
       const paymentMethod = validMethods.value?.find(p => p.id === id);
       await updatePaymentMethod({paymentMethod});
       emit('status');
+
     };
 
     onSSR(async () => {
@@ -75,21 +86,46 @@ export default {
     });
 
     return {
+      bamboraApplePayMethodId,
       methods: validMethods,
       selectedMethod,
       selectMethod,
       loading,
-      th
+      th,
+      isCreditCard
     };
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.payment {
-  &__label {
+.payment-methods {
+  border-bottom: solid 2px var(--c-light);
+  padding: 0 0 var(--spacer-base) 0;
+  margin-bottom: var(--spacer-lg);
+  @include for-desktop {
     display: flex;
     justify-content: space-between;
+    margin-bottom: var(--spacer-xl);
+    padding: var(--spacer-xs) 0;
+    width: 100%;
+  }
+}
+.payment-method {
+  --radio-container-align-items: center;
+  --radio-container-padding: var(--spacer-base) var(--spacer-sm) 0;
+  --ratio-content-margin: 0 0 0 var(--spacer-lg);
+  --radio-background: transparent;
+  color: var(--c-link);
+  white-space: nowrap;
+  ::v-deep .sf-image {
+    width: 3.125rem;
+  }
+  @include for-desktop {
+    --radio-container-padding: var(--spacer-sm);
+  }
+  &-applepay {
+     display: none;
   }
 }
 </style>
