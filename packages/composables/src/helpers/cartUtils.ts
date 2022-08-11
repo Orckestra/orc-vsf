ec35082f46f0b643a2bd4e83cd37146e52172a01
Ananchenko Olga <olyaan91@gmail.com>
2022-08-11 19:59:04 +0300

    lint update

--- packages/composables/src/helpers/cartUtils.ts
+++ packages/composables/src/helpers/cartUtils.ts
@@ -13,11 +13,11 @@ export const validateLineItem = (cart: Cart, cartItem: CartItem): boolean => {
     return message.severity === 'Error';
   };
 
-  const hasErrorMassage = cart.messages?.some(message => 
-    isErrorMessage(message) 
-    && isMessageForLineItem(message) 
-    && message.propertyBag.LineItemId.replace(/-/g, '').toUpperCase() == cartItem.id.replace(/-/g, '').toUpperCase());
-  
-  if(hasErrorMassage && (!cartItem.status  || cartItem.status.toUpperCase()  !== 'InStock'.toUpperCase())) return false;
+  const hasErrorMassage = cart.messages?.some(message =>
+    isErrorMessage(message) &&
+    isMessageForLineItem(message) &&
+    message.propertyBag.LineItemId.replace(/-/g, '').toUpperCase() == cartItem.id.replace(/-/g, '').toUpperCase());
+
+  if (hasErrorMassage && (!cartItem.status || cartItem.status.toUpperCase() !== 'InStock'.toUpperCase())) return false;
   return true;
 };
