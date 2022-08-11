cfd85f85bb1362c33522befb7453bf5d39272a8c
Ananchenko Olga <olyaan91@gmail.com>
2022-08-11 14:56:49 +0300

    updated test

--- packages/composables/src/helpers/cartUtils.spec.ts
+++ packages/composables/src/helpers/cartUtils.spec.ts
@@ -75,8 +75,6 @@ let lineItems =  [
 lineItem
 ];
 
-
-
 describe('validateLineItem', () => {
     test('WHEN_no_message_in_cart_SHOULD_return_all_valid_line_item', () => {
         // Arrange
@@ -86,18 +84,123 @@ describe('validateLineItem', () => {
         expect(filteredLineItems.length).toBe(lineItems.length);
     }),
     test('WHEN_lineItem_invalid_in_message_SHOULD_return_false', () => {
-        const invalidLineItemId = "239c3529-9751-4d5b-90cd-c6b0af2cbf71";
+        const lineItemId = "239c3529-9751-4d5b-90cd-c6b0af2cbf71";
         cart.messages = [{
             messageId: "239c3529-9751-4d5b-90cd-c6b0af2cbf74",
             severity: "Error",
             propertyBag:
                 {
                     EntityType: "LineItem",
-                    LineItemId: invalidLineItemId
+                    LineItemId: lineItemId
                 }
         }];
-        lineItem.id = invalidLineItemId;
+        lineItem.id = lineItemId;
         const isValid = validateLineItem(cart, lineItem);
         expect(isValid).toBe(false);
+    }),
+    test('WHEN_message_is_not_error_SHOULD_ignore_message', () => {
+        const lineItemId = "239c3529-9751-4d5b-90cd-c6b0af2cbf71";
+        cart.messages = [{
+            messageId: "239c3529-9751-4d5b-90cd-c6b0af2cbf74",
+            severity: "Unspecified",
+            propertyBag:
+                {
+                    EntityType: "LineItem",
+                    LineItemId: lineItemId
+                }
+        }];
+        lineItem.id = lineItemId;
+        const isValid = validateLineItem(cart, lineItem);
+        expect(isValid).toBe(true);
+    }),
+    test('WHEN_message_has_no_entityType_SHOULD_ignore_message', () => {
+        const lineItemId = "239c3529-9751-4d5b-90cd-c6b0af2cbf71";
+        cart.messages = [{
+            messageId: "239c3529-9751-4d5b-90cd-c6b0af2cbf74",
+            severity: "Unspecified",
+            propertyBag:
+                {
+                    LineItemId: lineItemId
+                }
+        }];
+        lineItem.id = lineItemId;
+        const isValid = validateLineItem(cart, lineItem);
+        expect(isValid).toBe(true);
+    }),
+    test('WHEN_message_has_other_entityType_than_lineitem_SHOULD_ignore_message', () => {
+        const lineItemId = "239c3529-9751-4d5b-90cd-c6b0af2cbf71";
+        cart.messages = [{
+            messageId: "239c3529-9751-4d5b-90cd-c6b0af2cbf74",
+            severity: "Unspecified",
+            propertyBag:
+                {
+                    EntityType: 12,
+                    LineItemId: lineItemId
+                }
+        }];
+        lineItem.id = lineItemId;
+        const isValid = validateLineItem(cart, lineItem);
+        expect(isValid).toBe(true);
+    }),
+    test('WHEN_message_has_no_lineItemId_SHOULD_ignore_message', () => {
+        const lineItemId = "239c3529-9751-4d5b-90cd-c6b0af2cbf71";
+        cart.messages = [{
+            messageId: "239c3529-9751-4d5b-90cd-c6b0af2cbf74",
+            severity: "Unspecified",
+            propertyBag:
+                {
+                    EntityType: "LineItem"
+                }
+        }];
+        lineItem.id = lineItemId;
+        const isValid = validateLineItem(cart, lineItem);
+        expect(isValid).toBe(true);
+    }),
+    test('WHEN_lineItem_is_not_marked_as_invalid_SHOULD_return_true', () => {
+        const lineItemId = "239c3529-9751-4d5b-90cd-c6b0af2cbf71";
+        cart.messages = [{
+            messageId: "239c3529-9751-4d5b-90cd-c6b0af2cbf74",
+            severity: "Unspecified",
+            propertyBag:
+                {
+                    EntityType: "LineItem",
+                    LineItemId: "339c3529-9751-4d5b-90cd-c6b0af2cbf71"
+                }
+        }];
+        lineItem.id = lineItemId;
+        const isValid = validateLineItem(cart, lineItem);
+        expect(isValid).toBe(true);
+    }),
+    test('WHEN_lineItem_is_not_in_stock_SHOULD_return_false', () => {
+        const lineItemId = "239c3529-9751-4d5b-90cd-c6b0af2cbf71";
+        cart.messages = [{
+            messageId: "239c3529-9751-4d5b-90cd-c6b0af2cbf74",
+            severity: "Unspecified",
+            propertyBag:
+                {
+                    EntityType: "LineItem",
+                    LineItemId: lineItemId
+                }
+        }];
+        lineItem.id = lineItemId;
+        lineItem.status = "Invalid status";
+        const isValid = validateLineItem(cart, lineItem);
+        expect(isValid).toBe(false);
+    }),
+    test('WHEN_lineItem_is_in_stock_and_not_marked_invalid_SHOULD_return_true', () => {
+        const lineItemId = "239c3529-9751-4d5b-90cd-c6b0af2cbf71";
+        cart.messages = [{
+            messageId: "239c3529-9751-4d5b-90cd-c6b0af2cbf74",
+            severity: "Unspecified",
+            propertyBag:
+                {
+                    EntityType: "LineItem",
+                    LineItemId: "239c3529-9751-4d5b-90cd-c6b0af2cbf74"
+                }
+        }];
+        lineItem.id = lineItemId;
+        lineItem.status = "InStock";
+        const isValid = validateLineItem(cart, lineItem);
+        expect(isValid).toBe(true);
     })
 });
