a5629c2b2de283eb2ef90f35f94887dda4adfb93
Ananchenko Olga <olyaan91@gmail.com>
2022-08-11 19:58:25 +0300

    removed grouping

--- packages/composables/src/helpers/cartUtils.ts
+++ packages/composables/src/helpers/cartUtils.ts
@@ -13,22 +13,11 @@ export const validateLineItem = (cart: Cart, cartItem: CartItem): boolean => {
     return message.severity === 'Error';
   };
 
-  const filteredLineItems = cart.messages?.filter(item => isErrorMessage(item) && isMessageForLineItem(item));
-  const erronousLineItems = filteredLineItems?.reduce((result, currentValue) => {
-    (result[currentValue.propertyBag.LineItemId] = result[currentValue.propertyBag.LineItemId] || []).push(
-      currentValue
-    );
-    return result;
-  }, {});
-
-  if (!erronousLineItems) {
-    return true;
-  }
-  const groups = Object.keys(erronousLineItems).map(key => ({
-    key: key,
-    value: erronousLineItems[key]
-  }));
-  const group = groups.find(item => item.key.replace(/-/g, '') === cartItem.id.replace(/-/g, ''));
-
-  return !(group && (!cartItem.status || cartItem.status !== 'InStock'));
+  const hasErrorMassage = cart.messages?.some(message => 
+    isErrorMessage(message) 
+    && isMessageForLineItem(message) 
+    && message.propertyBag.LineItemId.replace(/-/g, '').toUpperCase() == cartItem.id.replace(/-/g, '').toUpperCase());
+  
+  if(hasErrorMassage && (!cartItem.status  || cartItem.status.toUpperCase()  !== 'InStock'.toUpperCase())) return false;
+  return true;
 };
