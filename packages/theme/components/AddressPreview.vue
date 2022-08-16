f6cebca2992517c240f8040f7aa4f33c48d3ecb2
OlyaAn <59433752+OlyaAn@users.noreply.github.com>
2022-08-16 14:46:18 +0300

    order details not showing address name (#72)

--- packages/theme/components/AddressPreview.vue
+++ packages/theme/components/AddressPreview.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="address-preview" v-if="address">
     <span v-if="address.addressName && showAddressName" class="sf-property__value address-preview-name">{{address.addressName}}</span>
-    <span v-if="address.firstName && showName">{{`${address.firstName} ${address.lastName}`}}</span>
+    <span v-if="address.firstName && showName">{{`${address.firstName} ${address.lastName ? address.lastName : ''}`}}</span>
     <span v-if="address.city">{{`${address.city}, ${address.line1 ? address.line1 : ''} ${address.line2 ? address.line2 : ''}`}}</span>
     <span v-if="address.regionCode">{{`${countriesGetters.getCountryRegionName(countries, address.countryCode, address.regionCode)}, ${address.postalCode}`}}</span>
     <span v-if="address.countryCode">{{`${countriesGetters.getCountryName(countries, address.countryCode)}`}}</span>
