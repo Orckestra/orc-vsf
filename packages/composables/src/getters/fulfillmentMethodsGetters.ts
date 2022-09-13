3f5f67b81f58bb9eca86cd1f53888cef175cd9f1
Inna Boitsun <inna.boitsun@orckestra.com>
2022-09-13 14:01:02 +0300

    AB#65768

--- packages/composables/src/getters/fulfillmentMethodsGetters.ts
+++ packages/composables/src/getters/fulfillmentMethodsGetters.ts
@@ -5,11 +5,19 @@ function getFulfillmentMethod(fulfillmentMethods: FulfillmentMethod[], shippingP
   return fulfillmentMethods?.find(c => c.shippingProviderId === shippingProviderId);
 }
 
+function getFulfillmentMethodsGroupedByType(fulfillmentMethods: FulfillmentMethod[]): Record<string, FulfillmentMethod[]> {
+  return fulfillmentMethods?.reduce((rv, x) => {
+    (rv[x.fulfillmentMethodType] = rv[x.fulfillmentMethodType] || []).push(x);
+    return rv;
+  }, {});
+}
+
 function getFulfillmentMethodType(fulfillmentMethods: FulfillmentMethod[], shippingProviderId: string): FulfillmentMethodType {
   return this.getFulfillmentMethod(fulfillmentMethods, shippingProviderId)?.fulfillmentMethodType;
 }
 
 export const fulfillmentMethodsGetters: FulfillmentMethodsGetters<FulfillmentMethod> = {
   getFulfillmentMethod,
-  getFulfillmentMethodType
+  getFulfillmentMethodType,
+  getFulfillmentMethodsGroupedByType
 };
