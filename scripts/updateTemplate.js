53e6c39809e4b1c37698fbf59d48fa95b926ecd4
Taras Nakonechnyi <taras.nakonechnyi@orckestra.com>
2022-10-06 13:18:43 +0300

    updateTemplate.js

--- scripts/updateTemplate.js
+++ scripts/updateTemplate.js
@@ -1,14 +1,29 @@
 const path = require('path');
-const { inheritTheme } = require('../node_modules/@vue-storefront/cli/dist/domains/theme/index.js');
+const globby = require('globby');
+const rimraf = require('rimraf');
+const fse = require('fs-extra');
+const { inheritTheme } = require('@vue-storefront/cli/dist/domains/theme');
 
 const run = async () => {
   const integrationPath = path.join(__dirname, '../packages/theme/');
-  const projectPath = path.join(__dirname, '../../orc-vsf-template/');
+  const projectPath = path.join(__dirname, '../../orc-vsf-template-output/');
+  const templateRepoPath = path.join(__dirname, '../../orc-vsf-template');
 
   await inheritTheme({
     projectPath,
     integrationPath
   });
+
+  // remove old files, except .git and yarn.lock
+  globby([`${templateRepoPath}/**/*`, `${templateRepoPath}/**/.*`, `!${templateRepoPath}/yarn.lock`, `!${templateRepoPath}/.git`]).then(
+    function then(paths) {
+      paths.map(function map(item) {
+        rimraf.sync(item);
+      });
+    }
+  );
+
+  await fse.copy(projectPath, templateRepoPath, { overwrite: true });
 };
 
 run();
